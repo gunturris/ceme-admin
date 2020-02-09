4fdc06faa1645c7a198e76ce1214c5d84f5ccc4e
gunturris <gunturris@gmail.com>
2020-02-09 13:02:03 +0700

    Index Main

--- web/index.php
+++ web/index.php
@@ -1,6 +1,7 @@
 <?php
 ini_set("display_errors" , 1);
-require_once("../autoload.php");
+
+require_once(__DIR__ ."../autoload.php");
 
 $_GET['com'] = isset($_GET['com']) ? $_GET['com'] : DEFAULT_WEB_URL ;
  var_dump($_GET);
