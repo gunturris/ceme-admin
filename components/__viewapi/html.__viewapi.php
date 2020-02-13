2021ae72c2e05267814eafc598b7006712315747
Guntur Ristiarto <guntur@limapandawa.com>
2020-02-13 13:35:20 +0700

    Index test 76

--- components/__viewapi/html.__viewapi.php
+++ components/__viewapi/html.__viewapi.php
@@ -50,3 +50,66 @@ function button_icon( $icon , $urlproperty = false , $buttonproperty=false ){
 
 	return $view;
 }
+
+
+function big_button_icon( $icon , $urlproperty = false , $buttonproperty=false ){
+	$view ='';
+	if($urlproperty){
+		if(! is_array($urlproperty) ) return false;
+			if($urlproperty)
+				$view .='<a ';
+			
+			foreach($urlproperty as $key=>$value){
+				$view .= $key . '="' . $value.'" ';
+			}
+				
+			if($urlproperty)
+				$view .='>';
+	}	
+	
+		$view .='<img style="border:1px solid green;" src="http://'.$_SERVER['HTTP_HOST'].'/assets/icons/'.$icon.'" border="0" ';
+		if($buttonproperty){
+			if(is_array($buttonproperty)){
+				foreach($buttonproperty as $bkey=>$bvalue){
+					$view .= $bkey . '="' . $bvalue.'" ';
+				}
+			}
+		}
+		$view .='/>';
+	if($urlproperty) $view .='</a>';
+
+	return $view;
+}
+
+function button_icon_besar( $icon , $urlproperty = false , $buttonproperty=false ){
+	$view ='';
+	if($urlproperty){
+		if(! is_array($urlproperty) ) return false;
+			if($urlproperty)
+				$view .='<a ';
+			
+			foreach($urlproperty as $key=>$value){
+				$view .= $key . '="' . $value.'" ';
+			}
+				
+			if($urlproperty)
+				$view .='>';
+	}	
+	
+		$view .='<img style="margin:0 1px 0 2px;" width="22px" src="http://'.$_SERVER['HTTP_HOST'].'/assets/icons/'.$icon.'" border="0" ';
+		if($buttonproperty){
+			if(is_array($buttonproperty)){
+				foreach($buttonproperty as $bkey=>$bvalue){
+					$view .= $bkey . '="' . $bvalue.'" ';
+				}
+			}
+		}
+		$view .='/>';
+	if($urlproperty) $view .='</a>';
+
+	return $view;
+}
+ 
+function hari_kerja_lembur($karyawan_id , $date){
+	return false;
+}
