fdfbd8e9e49078ff3f3c2626d70924201cfa5df8
Guntur Ristiarto <guntur@limapandawa.com>
2020-02-13 09:57:38 +0700

    Index test12

--- autoload.php
+++ autoload.php
@@ -370,14 +370,12 @@ function my_direct( $page  ){
 
 function my_get_path_files(){
 	return MY_FILES_PATH;
-});
+}
  
 my_component_load('__system');
  
 my_component_load('__viewapi');  
-
+my_api_load(); 
 define( "my_load" , my_token() );
  
 var_dump($_SERVER);
-my_api_load(); 
-var_dump($_GET);         
