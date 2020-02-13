e9573c711bedeffdbe38e9839a414b99542f4f77
Guntur Ristiarto <guntur@limapandawa.com>
2020-02-13 09:44:26 +0700

    Index test9

--- web/index.php
+++ web/index.php
@@ -3,6 +3,7 @@ ini_set("diplay_errors" , 1);
 var_dump("AAAAA");
 include( "/var/www/html/ceme-admin/config.php");
 var_dump("BBBBBB");
+
 $_GET['com'] = isset($_GET['com']) ? $_GET['com'] : DEFAULT_WEB_URL ;
 var_dump("CCCC");
 var_dump($_GET['com']);
@@ -11,7 +12,7 @@ if(! isset($_SESSION['user_id'])){
 	my_direct('login.php');
 } 
 */
- 
+ exit;
 if( ! my_is_component( $_GET['com'] ) ){
     var_dump("DDDDD");
 	fatal_error('Module utama tidak ditemukan');
