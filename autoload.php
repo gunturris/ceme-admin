adae1652fa6d69a6fc49cdf5e6d22f51efbc376e
Guntur Ristiarto <guntur@limapandawa.com>
2020-02-13 10:36:29 +0700

    Index test39

--- autoload.php
+++ autoload.php
@@ -124,13 +124,15 @@ function my_api_load(){
 	 
 	$sets = get_api_file_list('settings');
     var_dump($sets);
-     require_once( '/var/www/html/ceme-admin/settings/api.form.php' );
-    var_dump("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
-     require_once( '/var/www/html/ceme-admin/settings/api.database.php' );
+    require_once( '/var/www/html/ceme-admin/settings/api.database.php' );
     var_dump("***************************************");
+    
+    //require_once( '/var/www/html/ceme-admin/settings/api.form.php' );
+    var_dump("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+    
     // require_once( '/var/www/html/ceme-admin/settings/api.table.php' );
     var_dump("++++++++++++++++++++++++");
-     require_once( '/var/www/html/ceme-admin/settings/api.permission.php' );
+    require_once( '/var/www/html/ceme-admin/settings/api.permission.php' );
     var_dump("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
     exit;
 	foreach( $sets as $filename ){
