050a2b50eb49b74305f056a858ab86b41d7e230c
Guntur Ristiarto <guntur@limapandawa.com>
2020-02-13 09:21:36 +0700

    Index test

--- web/index.php
+++ web/index.php
@@ -1,7 +1,7 @@
 <?php 
-
+var_dump("AAAAAAA");
 require_once(__DIR__ . "/../config.php");
-
+var_dump("BBBBBB");
 $_GET['com'] = isset($_GET['com']) ? $_GET['com'] : DEFAULT_WEB_URL ;
 
 /* 
@@ -14,5 +14,5 @@ if( ! my_is_component( $_GET['com'] ) ){
 	fatal_error('Module utama tidak ditemukan');
 }  
  
-
+var_dump($_GET);
 my_exec($_GET['com'] ); 
