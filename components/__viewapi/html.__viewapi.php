af00258c10319bd1dd31d9178e54db9a976b8c21
Guntur Ristiarto <guntur@limapandawa.com>
2020-02-13 13:32:43 +0700

    Index test 75

--- components/__viewapi/html.__viewapi.php
+++ components/__viewapi/html.__viewapi.php
@@ -10,3 +10,13 @@ function get_range_date_of_month($year , $month){
     $datas['end_date'] = date("Y-m-t" , strtotime($initDate ));
     return $datas;
 }
+
+
+
+function is_time_format($time){
+    return preg_match('#^([01]?[0-9]|2[0-3]):[0-5][0-9](:[0-5][0-9])?$#', $time);
+}
+ 
+function symbol_breadcumb(){
+	return '&nbsp; '.button_icon( 'b_nextpage.png' ,array() ).' &nbsp; ';
+}
