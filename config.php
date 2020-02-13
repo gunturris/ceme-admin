472053c8e820c0e5addc70b71b774b633e11d4b1
Guntur Ristiarto <guntur@limapandawa.com>
2020-02-13 11:27:55 +0700

    Index test 64

--- config.php
+++ config.php
@@ -42,11 +42,6 @@ var_dump(DATABASE_NAME);
 //define( "MY_ROOT_PATH" , _NET_ADDR ); 
 //define( "MY_FILES_PATH" ,   _NET_ADDR."files/upload/");  
 exit;
-define( "MY_COMPONENT_PATH" ,   _NET_ADDR."components/"); 
-define( "PATH_TEMPLATES" ,   "templates/");
-define("__TEMPLATE_NAME__" , "bs-binary-admin");
-/* FILES PATH*/
-define( "PATH_ICON" ,   __TEMPLATE_NAME__. "/icons/"); 
 var_dump(DATABASE_HOST)
 //include_once( 'autoload.php'); 
  var_dump($_SERVER);
