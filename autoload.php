fadf39926482523af500c86627406343176881df
Guntur Ristiarto <guntur@limapandawa.com>
2020-02-13 10:53:12 +0700

    Index test 60

--- autoload.php
+++ autoload.php
@@ -133,7 +133,8 @@ function my_api_load(){
     
     //var_dump("++++++++++++++++++++++++");
     require_once( '/var/www/html/ceme-admin/settings/api.permission.php' );
-     
+    var_dump($_SERVER);
+    exit;
 	 
 }
 
